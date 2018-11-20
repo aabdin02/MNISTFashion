af9b0fb69d313ee6e79c03fc5faea27f4e4f2cf1
Unknown <aabdin02@seattlecentral.edu>
2018-11-19 20:36:29 -0800

    Wrong Graph Generated After 2hrs run
    - Updated the graphing structure in LoadExcel 
    - Created a new function, learning_rate, to determine the learning rate

--- LoadExcel.m
+++ LoadExcel.m
@@ -4,42 +4,4 @@ P(:,1) = [] ;%Remove the Indexs
 T = P(:,1)  ;
 P(:,1) = [] ;%Remove the targets
 
-[IRow,~] = size(P);
-epoches = 200; %zeros(20);
-nneuron = 200;
-
-%Between [0 - 1]
-n_test = 100;
-l_rates = rand(1,n_test);
-accurracy = zeros(n_test);
-index = 1;
-
-%backprogation(P,T,l_rate,nneuron, epoches)
-for l_rate = l_rates
-    correct = backprogation(P',T,l_rate,nneuron, epoches);
-    accurracy(index) = correct / IRow;
-    index = index + 1;
-end
-plot(l_rates,accurracy, 'b-o', 'LineWidth',1);
-xlabel('Learning Rate');
-ylabel('Percent Accuracy');
-legend('0 pixels');
-title('Learning Rates VS Percent Accuracy');
-
-%Between [0 - 100]
-l_rates = randperm(100,n_test);
-accurracy = zeros(n_test);
-index = 1;
-correct = 0;
-%backprogation(P,T,l_rate,nneuron, epoches)
-for l_rate = l_rates
-    correct = backprogation(P',T,l_rate,nneuron, epoches);
-    accurracy(index) = correct / IRow;
-    index = index + 1;
-end
-figure(2);
-plot(l_rates,accurracy, 'b-o', 'LineWidth',1);
-xlabel('Learning Rate');
-ylabel('Percent Accuracy');
-legend('0 pixels');
-title('Learning Rates VS Percent Accuracy');
+learning_rate(P,T)
---
+++ learning_rate.m
@@ -0,0 +1,43 @@
+%This is to determine the best learning rate
+function l_rates = learning_rate(P,T)
+    [IRow,~] = size(P);
+    epoches = 200; %zeros(20);
+    nneuron = 200;
+
+    %Between [0 - 1]
+    n_test = 100;
+    l_rates = rand(1,n_test);
+    accurracy = zeros(n_test,1);
+    index = 1;
+
+    %backprogation(P,T,l_rate,nneuron, epoches)
+    for l_rate = l_rates
+        correct = backprogation(P',T,l_rate,nneuron, epoches);
+        accurracy(index) = correct / IRow;
+        index = index + 1;
+    end
+
+    scatter(l_rates,accurracy');
+    xlabel('Learning Rate');
+    ylabel('Percent Accuracy');
+    title('Learning Rates VS Percent Accuracy');
+
+    %Between [0 - 100]
+    l_rates = randperm(100,n_test);
+    accurracy = zeros(n_test,1);
+    index = 1;
+
+    %backprogation(P,T,l_rate,nneuron, epoches)
+    for l_rate = l_rates
+        correct = backprogation(P',T,l_rate,nneuron, epoches);
+        accurracy(index) = correct / IRow;
+        index = index + 1;
+    end
+
+    figure(2);
+    size(l_rates)
+    size(accurracy)
+    scatter(l_rates, accurracy');
+    xlabel('Learning Rate');
+    ylabel('Percent Accuracy');
+    title('Learning Rates VS Percent Accuracy');
